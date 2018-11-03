20267d3263d1fa824aee8f67e0ab03b5af92e55c
NastuzziSamy <samy@nastuzzi.fr>
2018-11-03 13:35:13 +0100

    Add fake fields (which are attributes not in database but callable)

---
+++ src/FakeFields/BelongsToField.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace NastuzziSamy\Laravel\FakeFields;
+
+use NastuzziSamy\Laravel\Traits\StaticCallable;
+
+class BelongsToField extends FakeField
+{
+    protected $off;
+    protected $to;
+    protected $on;
+
+    public function __construct(string $model = null, string $column = 'id', string $fromColumn = null)
+    {
+        $this->off = $fromColumn;
+        $this->to = $model;
+        $this->on = $column;
+
+        parent::__construct();
+    }
+
+    protected function to(string $model) {
+        $this->_checkLock();
+
+        $this->to = $model;
+    }
+
+    protected function on(string $column) {
+        $this->_checkLock();
+
+        $this->on = $column;
+    }
+
+    protected function from(string $column) {
+        $this->_checkLock();
+
+        $this->off = $column;
+    }
+
+    public function get($model) {
+        $this->relateToModel($model)->first();
+    }
+
+    public function getFieldName() {
+        return $this->off;
+    }
+
+    public function relateToModel($model) {
+        return $model->belongsTo($this->to, $this->off, $this->on);
+    }
+
+    public function scopeWhere($model, ...$args) {
+        return $model->where($this->columnName, ...$args);
+    }
+}
---
+++ src/FakeFields/FakeField.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace NastuzziSamy\Laravel\FakeFields;
+
+use NastuzziSamy\Laravel\Traits\StaticCallable;
+
+abstract class FakeField
+{
+    use StaticCallable;
+
+    protected $name;
+
+    protected $locked = false;
+
+    protected function __construct() {
+
+    }
+
+    protected function _name($value) {
+        $this->_checkLock();
+
+        $this->name = $value;
+
+        return $this;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function getFieldName() {
+        return $this->name;
+    }
+
+    protected function lock(string $name) {
+        $this->_name($name);
+
+        $this->locked = true;
+    }
+
+    protected function _checkLock() {
+        if ($this->locked) {
+            throw new \Exception('The field is locked, nothing can change');
+        }
+    }
+
+    public function call($model, ...$args) {
+        if (count($args) === 0) {
+            return $this->relatedToModel($model);
+        }
+        else {
+            return $this->scopeWhere($model, ...$args);
+        }
+    }
+
+    abstract public function get($model);
+    abstract public function relateToModel($model);
+    abstract public function scopeWhere($model, ...$args);
+}
